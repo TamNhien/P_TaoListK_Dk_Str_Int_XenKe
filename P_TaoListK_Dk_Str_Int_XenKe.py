fc05c32b7d4b709943f0d1d371f1bc15d7649ef5
TamNhien <canhthien91@gmail.com>
2024-03-28 21:00:12 +0700

    Add project files.

---
+++ P_TaoListK_Dk_Str_Int_XenKe.py
@@ -0,0 +1,7 @@
+﻿L = input("Nhập vào danh sách các chuỗi và số nguyên, cách nhau bởi dấu phẩy: ").split(',')
+
+if all(type(L[i]) != type(L[i + 1]) for i in range(len(L) - 1)):
+    K = [str(L[i] * L[i + 1]) for i in range(0, len(L), 2)]
+    print("List K mới:", K)
+else:
+    print("Các phần tử trong danh sách L không xen kẽ nhau.")
